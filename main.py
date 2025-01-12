036d31ae9a8185d73249bf84fd70b5800454dade
Dfox <Dfox@SILLYBOI.(none)>
2025-01-11 18:51:44 -0600

    basic pygame integration. create screen, add FPS controll, create game loop

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+import pygame
+import sys
 from Deck import Deck
 from Column import Column
 
+#init pygame
+pygame.init()
+
+#set screen properties
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pygame Template")
+
+#init clock
+clock = pygame.time.Clock()
+FPS = 60
+
+#create deck and 7 columns with only last card of column face up
 deck_main = Deck()
 row = []
 for ammount in range(8):
     row.append(Column(ammount + 1, deck_main))
     row[ammount].contents[-1].is_face_up = True
+
+
+#main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # update display
+    pygame.display.flip()
+
+    # control frame rate
+    clock.tick(FPS)
+
+
 
 #debug
 # for column in row:
