82a0236b8e03b2f12e0e00a9ea61148c6dc5a99a
Dfox <Dfox@SILLYBOI.(none)>
2025-01-11 18:37:55 -0600

    make last card in each column face up

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ deck_main = Deck()
 row = []
 for ammount in range(8):
     row.append(Column(ammount + 1, deck_main))
+    row[ammount].contents[-1].is_face_up = True
 
+#debug
 # for column in row:
 #     print("\n")
 #     for card in column.contents:
-#         print(str(card.value) + str(card.suit))
+#         print(str(card.value) + str(card.suit) + str(card.is_face_up))
 
 # for card in deck_main.contents:
-#     print(str(card.value) + str(card.suit))
+#     print(str(card.value) + str(card.suit) + str(card.is_face_up))
