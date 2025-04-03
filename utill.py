c47d2e5c0b08b340d4419924f17a02eef46a5edc
DeadFoxx1 <leo.a09@icloud.com>
2025-04-02 21:00:33 -0500

    formatting?

--- utill.py
+++ utill.py
@@ -2,16 +2,18 @@ import pygame
 
 card_cache = None
 
-#run at creation of game but will not loop
+
+# run at creation of game but will not loop
 def game_init():
     from obj import top_row, bottom_row
-    #init pygame
+
+    # init pygame
     pygame.init()
 
-    #pygame caption
+    # pygame caption
     pygame.display.set_caption("Pygame Template")
 
-    #debug: print attributes of each card in each column 
+    # debug: print attributes of each card in each column
     for column in top_row.contents:
         for card in column.contents:
             print(card)
@@ -22,16 +24,20 @@ def game_init():
             print(card)
         print("\n")
 
+
 def display_rows():
     from obj import bottom_row, top_row
+
     bottom_row.display()
     top_row.display()
+
 
 def clear_select_cards(selected_card):
     global card_cache
     selected_card[1].is_selected = False
     card_cache[1].is_selected = False
     card_cache = None
+
 
 def select_card(pos: "event.pos"):
     global card_cache
@@ -55,20 +61,29 @@ def select_card(pos: "event.pos"):
     else:
         card_cache = selected_card
 
+
 def move_card(card_destination):
     global card_cache
-    cards_to_move = card_cache[0].contents[card_cache[0].contents.index(card_cache[1]):]
+    cards_to_move = card_cache[0].contents[
+        card_cache[0].contents.index(card_cache[1]) :
+    ]
     destination_column = card_destination[0].contents
 
-        #if the color is opposite and the destination is on more 
-        #OR if the destination is a foundation, the suit is the same, and the value is one more then in the foundation, 
-        #move
-    if  (destination_column[-1].is_face_up and card_cache[1].is_face_up) and \
-        (destination_column[-1].color != card_cache[1].color and \
-        destination_column[-1].value == card_cache[1].value + 1) or \
-        (destination_column[0].value == 0 and \
-        destination_column[-1].value == card_cache[1].value - 1 and \
-        destination_column[-1].suit == card_cache[1].suit):
+    # if the color is opposite and the destination is on more
+    # OR if the destination is a foundation, the suit is the same, and the value is one more then in the foundation,
+    # move
+    if (
+        (destination_column[-1].is_face_up and card_cache[1].is_face_up)
+        and (
+            destination_column[-1].color != card_cache[1].color
+            and destination_column[-1].value == card_cache[1].value + 1
+        )
+        or (
+            destination_column[0].value == 0
+            and destination_column[-1].value == card_cache[1].value - 1
+            and destination_column[-1].suit == card_cache[1].suit
+        )
+    ):
 
         print(f"move {card_cache[1]} to {card_destination[1]}")
         for card in cards_to_move:
@@ -78,8 +93,10 @@ def move_card(card_destination):
             card_cache[0].contents[-1].is_face_up = True
     else:
         print("invalid move")
-    
+
+
 def update_card():
     from obj import top_row, bottom_row
+
     top_row.update_card()
     bottom_row.update_card()
