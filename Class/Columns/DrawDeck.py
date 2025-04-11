0a8075cc105ed8531b14120ad24bfac45910ee5b
DeadFoxx1 <DeadFoxx1@users.noreply.github.com>
2025-04-11 17:46:55 -0500

    add a new attribute to Column to verify cards are allowed to move to them. (Fix issue where cards could return to the draw deck)

--- Class/Columns/DrawDeck.py
+++ Class/Columns/DrawDeck.py
@@ -8,7 +8,7 @@ class DrawDeck(Column):
         num_of_cards: "int: pass one less then intended amount",
         deck: "Deck object",
     ):
-        super().__init__(num_of_cards, deck)
+        super().__init__(num_of_cards, deck, False)
         self.contents.insert(0, Card(0, 0, True))
 
     def select_card(self, pos):
