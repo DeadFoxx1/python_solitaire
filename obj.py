6288c2bb6ed1513978f21ecf6be5010a973f0460
DeadFoxx1 <206004751+DeadFoxx1@users.noreply.github.com>
2025-04-12 18:09:50 -0500

    clean up obj

--- obj.py
+++ obj.py
@@ -6,20 +6,6 @@ from Class.Rows.TopRow import TopRow
 from Class.Rows.BottomRow import BottomRow
 from Class.Card import Card
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--debug", type=bool, default=False)
-
-
-# create necessary pygame objects for screen and fps control
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
-
-# create deck
-# deck_main = Deck()
-
-# create second deck with just foundation cards
-# foundation = Deck(True)
-
 
 def create_shuffled_deck():
     deck = [Card(suit, value, False) for value in range(1, 14) for suit in SUITS]
@@ -27,7 +13,12 @@ def create_shuffled_deck():
     return deck
 
 
-# main deck
+parser = argparse.ArgumentParser()
+parser.add_argument("--debug", type=bool, default=False)
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
 deck = create_shuffled_deck()
 
 foundation_deck = [Card(suit, 0, True) for suit in SUITS]
